6c80dd6e26811781de6e5aa93c39a16782952248
mohitsharma012 <stayer.mohit@gmail.com>
2025-03-17 09:17:10 +0530

    add @types/mobile-detect to devDependencies

--- client/utils/detectMobile.ts
+++ client/utils/detectMobile.ts
@@ -1,7 +1,6 @@
 import MobileDetect from "mobile-detect";
 
-export const isMobileDevice = (userAgent) => {
-    // const userAgent = navigator.userAgent;
+export const isMobileDevice = (userAgent: string) => {
     const md = new MobileDetect(userAgent);
     return !!md.mobile();
 };
